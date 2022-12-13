dd43c5577ce074b54450608da0e20defef990269
Kyuwoong Lee <kwooler@gmail.com>
2022-12-12 22:47:59 -0800

    Update A64.hpp

--- A64.hpp
+++ A64.hpp
@@ -14,4 +14,16 @@ using namespace std;
 
 void getListprime(int begin, int end)
 {
+  int i, j;
+	if (begin <= 1 || end <= 1)
+		return;
+	for (i = begin; i < end; i++)
+	{
+		for (j = 2; j <= (i / j); j++)
+			if (i % j == 0)
+				break;
+		if (j > (i / j))
+			cout << i << " \t";
+	}
+	cout << endl;
 }
